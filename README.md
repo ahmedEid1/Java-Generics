5169dbf8c5068d778542f3f2c08141c864b95f0d
ahmed_eid <kingmedo954@gmail.com>
2020-12-24 20:51:31 +0200

    implementing a CircularBuffer

---
+++ README.md
@@ -0,0 +1,4 @@
+Collections can contain any Object
+
+Generics stops runtime error at compile time
+
---
+++ src/CircularBuffer.java
@@ -0,0 +1,34 @@
+public class CircularBuffer {
+    private Object[] buffer;
+    private int readCursor = 0;
+    private int writeCursor= 0;
+
+    public CircularBuffer(int size) {
+        buffer = new Object[size];
+    }
+
+    public boolean offer(Object value) {
+        if (buffer[writeCursor] != null)
+            return false;
+
+        buffer[writeCursor] = value;
+        writeCursor = next(writeCursor);
+
+        return true;
+    }
+
+    public Object poll() {
+        Object value = buffer[readCursor];
+        if (value != null) {
+            buffer[readCursor] = null;
+            readCursor = next(readCursor);
+
+        }
+        return value;
+    }
+
+    private int next(int index) {
+        return (index + 1) % buffer.length;
+    }
+
+}
